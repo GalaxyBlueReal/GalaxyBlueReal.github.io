39032403f5e911bc66d4ae48842e90f49bcc8fd7
GalaxyBlueReal <40841775+GalaxyBlueReal@users.noreply.github.com>
2023-05-03 19:41:05 -0400

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,3 +1,3 @@
 alert("Hello I am Whopper!");
 alert("I have now taken control >:3")
-document.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+window.top.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
